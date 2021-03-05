e4c7f2a8672004e9ca419d17c4153cc3cc3ad5a8
Codevian Developer <developer.codevian@gmail.com>
2021-03-05 18:39:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react'
-import Login from './Components/Login'
-import SignUp from './Components/SignUp'
+import Login from './pages/Login/Login'
+import SignUp from './pages/Register/Register'
+import Datatable from './pages/Datatable/Datatable'
+import PureComp from './pages/PureComp/PureComp';
+
+import {
+  BrowserRouter,
+  Switch,
+  Route 
+  
+} from "react-router-dom";
+import Header from './Components/Header/Header';
+
 
 function App() {
   return (
     <div>
-
-      <Login/>
-      <SignUp />
-
+     <BrowserRouter> 
+     <Header/>
+          <Switch>
+        {/*     
+            <Route exact path="/" component={Datatable} /> */}
+            <Route path="/Login" component={Login} />
+            <Route path="/SignUp" component={SignUp}/>
+            <Route path="/Datatable" component={Datatable}/>
+            <Route path="/PureComp" component={PureComp}/>
+          
+          </Switch>
+     
+     </BrowserRouter>
          
     </div>
   )
